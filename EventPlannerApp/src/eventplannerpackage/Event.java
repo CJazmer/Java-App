b430500cbf8bb41f88126e6e03d4fc2c524e2afb
CJazmer <125234435+CJazmer@users.noreply.github.com>
2026-03-23 13:09:04 -0500

    Cjazmer and Coezr05 Edits
    Cjazmer - basic layout setup / packages
    Coezr05 - more event properties, event lists, more functions

---
+++ EventPlannerApp/src/eventplannerpackage/Event.java
@@ -0,0 +1,104 @@
+package eventplannerpackage;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// name, description, totalPeople, totalCost... 
+public class Event {
+	String name;
+	String descrition;
+	ArrayList<String> people = new ArrayList<String>();
+	HashMap<String, Integer> expenses = new HashMap<>();
+
+	public Event(String name){
+		this.name = name;
+	}
+	
+		
+	
+	// Functions
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String n){
+		this.name = n;
+	}
+	public String getDescription(){
+		return this.descrition;
+	}
+	public void setDescription(String d){
+		this.descrition = d;
+	}
+	
+	//Functions specific to People
+	public ArrayList<String> getPeople(){
+		return this.people;
+	}
+	public void addPerson(String p){
+		int i = findPerson(p);
+		if( i!= -1) {
+			System.out.print("Person already in event");
+		}
+		else {
+			this.people.add(p);
+		}
+	}
+	public int findPerson(String p) {
+		int pos = -1;
+		for (int i = 0; i < people.size(); i++) {
+			if (this.people.get(i) == p) {
+				pos = i;
+			}
+		}
+		return pos;
+	}
+	public void editPerson(String oldP, String newP) {
+		int i = findPerson(oldP);
+		if( i== -1) {
+			System.out.print("Person not in list");
+		}
+		else {
+			this.people.set(i, newP);
+		}
+	}
+	public void removePerson(String p) {
+		int i = findPerson(p);
+		if( i== -1) {
+			System.out.print("Person not in list");
+		}
+		else {
+			this.people.remove(i);
+		}
+	}
+	public int getTotalPeople() {
+		return this.people.size();
+	}
+	
+	//Functions specific to Expenses
+	public HashMap<String, Integer> getExpenses() {
+		return this.expenses;
+	}
+	public void addExpense(String e, int c) {
+		if(this.expenses.containsKey(e)) {
+			System.out.print("Expense already in event");
+		}
+		else {
+			this.expenses.put(e, c);
+		}
+	}
+	public void removeExpense(String e) {
+		this.expenses.remove(e);
+	}
+	public void editExpense(String oldE, String newE, int c) {
+		if(oldE!=newE) {
+			this.expenses.remove(oldE);
+		}
+		this.expenses.put(newE, c);
+	}
+	public int getTotalCost() {
+		int tot=0;
+		for (int i : expenses.values()) {
+			  tot+=i;
+			}
+		return tot;
+	}
+}
---
+++ EventPlannerApp/src/eventplannerpackage/Screen.java
@@ -0,0 +1,75 @@
+package eventplannerpackage;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.RadioButton;
+import java.util.ArrayList;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+
+// Makes Screen Pop Up =====================================================
+public class Screen extends Application {
+	@Override
+	public void start(Stage screen) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Screen.fxml"));
+        Scene scene = new Scene(root);
+        screen.setScene(scene);
+        screen.show();
+	}
+	
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    
+    
+    
+    // Code =======================================================================
+    
+    ArrayList<Event> events = new ArrayList<Event>();
+    
+    @FXML
+    private VBox eventsBox;
+
+    @FXML
+    private TextField descriptionTextField;
+   
+	@FXML
+	private TextField nameTextField;    
+
+    
+	@FXML
+	void saveButtonPressed(ActionEvent event) {
+		
+		Event newEvent = new Event(nameTextField.getText());
+    	
+    	Button newButton = new Button(nameTextField.getText());
+    	eventsBox.getChildren().add(newButton);
+    	
+    	if(descriptionTextField.getText() != "") {
+    		newEvent.setDescription(descriptionTextField.getText());
+    	}
+    	
+    	events.add(newEvent);
+    	newButton.setOnAction(e -> {
+    		showEvent(newEvent);
+        });
+	}
+	
+	
+	private void showEvent(Event e) {
+		nameTextField.setText(e.getName());
+		descriptionTextField.setText(e.getDescription());    
+        
+	} 
+}
